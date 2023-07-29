909697f54cfd0e8ec41c18d0e66a0fbf42106099
atanusenstudy <atanusenstudy@gmail.com>
2023-07-29 23:19:59 +0530

    Implemented Lazy Loading

--- BaseUI/src/app/app-routing.module.ts
+++ BaseUI/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: 'customer', loadChildren: () => import('./customer-data/customer-data.module').then(m => m.CustomerDataModule) },
+  { path: 'product-data', loadChildren: () => import('./product-data/product-data.module').then(m => m.ProductDataModule) },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
